9cb288e08e9a7a34dd90dcdde8f88799ac4db015
manali141 <manali.mistry01@gmail.com>
2026-04-16 10:22:19 -0400

    Image path change

--- Script/homeScript.js
+++ Script/homeScript.js
@@ -3,21 +3,21 @@ const cardElemnt = [
         type: 'Wedding',
         title:'Classic Elegance',
         description:'A stunning three-tier vanilla bean cake with soft buttercream and fresh roses.',
-        imgUrl:'https://2987f4e8-4402-44f6-a52d-0410a55b4f7c-00-29dpsxyxmtkz5.riker.replit.dev/images/cake-1.png',
+        imgUrl:'https://manali141.github.io/SweetLayers/Images/cake-1.png',
         altText:'Wedding Cake with three tier vanila bean cake'
      },
      {
         type: 'Birthday',
         title:'Midnight Fudge',
         description:'Decadent chocolate layers with dark fudge frosting and chocolate curls.',
-        imgUrl:'https://2987f4e8-4402-44f6-a52d-0410a55b4f7c-00-29dpsxyxmtkz5.riker.replit.dev/images/cake-2.png',
+        imgUrl:'https://manali141.github.io/SweetLayers/Images/cake-2.png',
         altText:'Birthday Cake with chocolate layer'
      },
      {
         type:'Birthday',
         title:'Strawberry Dream',
         description:'Light sponge cake layered with fresh whipped cream and ripe strawberries',
-        imgUrl:'https://2987f4e8-4402-44f6-a52d-0410a55b4f7c-00-29dpsxyxmtkz5.riker.replit.dev/images/cake-3.png',
+        imgUrl:'https://manali141.github.io/SweetLayers/Images/cake-3.png',
         altText:'Strawberry sponge cake'
      },
 
